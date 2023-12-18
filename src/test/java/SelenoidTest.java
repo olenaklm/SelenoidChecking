533953a603cc1e1f422bab8431e9845f5fe433f3
Olena Klymenko <lenaklymenko9@gmail.com>
2023-12-18 22:57:25 +0200

    Lesson 32

--- src/test/java/SelenoidTest.java
+++ src/test/java/SelenoidTest.java
@@ -3,6 +3,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -22,7 +23,7 @@ public class SelenoidTest {
 
 //        WebDriver driver = new ChromeDriver();
 
-        /////////////////////////////////////////
+        //--
         ChromeOptions options = new ChromeOptions();
         options.setCapability("browserVersion", "latest");
         options.setCapability("selenoid:options", new HashMap<String, Object>() {{
@@ -30,7 +31,7 @@ public class SelenoidTest {
             put("name", "Test badge...");
 
             /* How to set session timeout */
-//            put("sessionTimeout", "15m");
+            put("sessionTimeout", "15m");
 
             /* How to set timezone */
             put("env", new ArrayList<String>() {{
@@ -46,7 +47,7 @@ public class SelenoidTest {
             put("enableVideo", false);
         }});
         RemoteWebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), options);
-        /////////////////////////////////////////
+        //--
 
         driver.manage().window().maximize();
 
@@ -57,12 +58,13 @@ public class SelenoidTest {
         driver.findElement(By.name("q")).sendKeys("hillel it school");
 
         driver.findElement(By.xpath("//div[@class='FPdoLc lJ9FBc']/center/input[@name='btnK']")).click();
+//        driver.findElement(By.xpath("//*[@name='btnK']")).click();
         Thread.sleep(3000);
         List<WebElement> elements = driver.findElements(By.xpath("//a/h3"));
         int i = 0;
         for (WebElement element : elements) {
             if (!element.getText().toLowerCase().contains("hillel it school")) {
-                System.out.println("Element with number " + i + " " + element.getText() + " doesn't containe \"hillel it school\"");
+                System.out.println("Element with number " + i + " " + element.getText() + " doesn't contain \"hillel it school\"");
             }
             i++;
         }
@@ -109,64 +111,13 @@ public class SelenoidTest {
         driver.findElement(By.name("q")).sendKeys("hillel it school");
 
         driver.findElement(By.xpath("//div[@class='FPdoLc lJ9FBc']/center/input[@name='btnK']")).click();
+//        driver.findElement(By.xpath("//*[@name='btnK']")).click();
         Thread.sleep(3000);
         List<WebElement> elements = driver.findElements(By.xpath("//a/h3"));
         int i = 0;
         for (WebElement element : elements) {
-            if (!element.getText().toLowerCase().contains("Linkedin")) {
-                System.out.println("Element with number " + i + " " + element.getText() + " doesn't containe \"Linkedin\"");
-            }
-            i++;
-        }
-        driver.quit();
-    }
-
-    @Test
-    public void Test3() throws InterruptedException, MalformedURLException {
-
-//        WebDriver driver = new FirefoxDriver();
-
-        /////////////////////////////////////////
-        FirefoxOptions options = new FirefoxOptions();
-        options.setCapability("browserVersion", "latest");
-        options.setCapability("selenoid:options", new HashMap<String, Object>() {{
-            /* How to add test badge */
-            put("name", "Test badge...");
-
-            /* How to set session timeout */
-//            put("sessionTimeout", "15m");
-
-            /* How to set timezone */
-            put("env", new ArrayList<String>() {{
-                add("TZ=UTC");
-            }});
-
-            /* How to add "trash" button */
-            put("labels", new HashMap<String, Object>() {{
-                put("manual", "true");
-            }});
-
-            /* How to enable video recording */
-            put("enableVideo", false);
-        }});
-        RemoteWebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), options);
-        /////////////////////////////////////////
-
-        driver.manage().window().maximize();
-
-        driver.get("https://www.google.com/?hl=en-US");
-
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[normalize-space()='Accept all']"))).click();
-        driver.findElement(By.name("q")).sendKeys("hillel it school");
-
-        driver.findElement(By.xpath("//div[@class='FPdoLc lJ9FBc']/center/input[@name='btnK']")).click();
-        Thread.sleep(3000);
-        List<WebElement> elements = driver.findElements(By.xpath("//a/h3"));
-        int i = 0;
-        for (WebElement element : elements) {
-            if (!element.getText().toLowerCase().contains("Linkedin")) {
-                System.out.println("Element with number " + i + " " + element.getText() + " doesn't containe \"Linkedin\"");
+            if (!element.getText().toLowerCase().contains("hillel it school")) {
+                System.out.println("Element with number " + i + " " + element.getText() + " doesn't contain \"Linkedin\"");
             }
             i++;
         }
